aa591bad2d65ac5049f3f1718dba3baec4917cc9
GergelyKarl <57073412+GergelyKarl@users.noreply.github.com>
2021-10-07 11:15:10 +0200

    0.6 new user widgets

--- src/components/widgets/WidgetsTrans.js
+++ src/components/widgets/WidgetsTrans.js
@@ -2,7 +2,7 @@ import React from 'react'
 import "./WidgetsTrans.css"
 export default function WidgetsTrans() {
     return (
-        <div className="widgets__trans">
+        <div className="widgets__transactions">
             dfg
         </div>
     )
--- src/components/widgets/WidgetsUsers.js
+++ src/components/widgets/WidgetsUsers.js
@@ -1,14 +1,32 @@
-import React from 'react'
-import "./WidgetsUsers.css"
+import React from "react";
+import "./WidgetsUsers.css";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+
 export default function WidgetsUsers() {
-    return (
-        <div className="widget__users">
-            <span className="widget__users__title"></span>
-            <ul className="widget__users__list">
-                <li className="widget__users__item">
-                    <img src="" alt="" className="widget__users__image" />
-                </li>
-            </ul>
-        </div>
-    )
+  return (
+    <div className="widget__users">
+      <span className="widget__users__title"></span>
+      <ul className="widget__users__list">
+        <li className="widget__users__item">
+          <img
+            src="https://images.unsplash.com/photo-1518577915332-c2a19f149a75?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=562&q=80"
+            alt=""
+            className="widget__users__image"
+          />
+          <div className="widget__users__user">
+            <span className="widget__users__name">
+              Mock girl1
+            </span>
+            <span className="widget__users__userTitle">
+              Recruiter
+            </span>
+          </div>
+          <button className="widget__users__button">
+            <VisibilityIcon />
+            Display
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
 }
